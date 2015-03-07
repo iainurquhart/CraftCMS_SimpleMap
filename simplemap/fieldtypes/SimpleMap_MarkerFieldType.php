4b2de27f046677a11ed34bae60827a0e2083eb8b
Iain Urquhart <iain@jamdigital.co.nz>
2015-03-07 16:00:29 +1300

    Bring in correct code, ahem.

--- simplemap/fieldtypes/SimpleMap_MarkerFieldType.php
+++ simplemap/fieldtypes/SimpleMap_MarkerFieldType.php
@@ -15,11 +15,9 @@ class SimpleMap_MarkerFieldType extends BaseFieldType
 		$id = craft()->templates->formatInputId($name);
 		$namespacedId = craft()->templates->namespaceInputId($id);
 
+		$this->addAssets();
 
-		craft()->templates->includeJsFile('//maps.googleapis.com/maps/api/js?sensor=false&libraries=places');
-		craft()->templates->includeJsResource('simplemap/js/SimpleMarker.js');
-		craft()->templates->includeCssResource('simplemap/css/global.css');
-		// craft()->templates->includeJs("$('#{$namespacedId}-field').simpleMap();");
+		craft()->templates->includeJs("$(function() { $('#{$namespacedId}-map-wrapper').simpleMap(); });");
 
 		return craft()->templates->render(
 			'simplemap/marker/input', array(
@@ -44,9 +42,15 @@ class SimpleMap_MarkerFieldType extends BaseFieldType
 	public function getSettingsHtml()
 	{
 
+		$namespacedId = craft()->templates->namespaceInputId('');
+
+		$this->addAssets();
+		craft()->templates->includeJs("$(function() { $('#{$namespacedId}map-wrapper').simpleMap(); });");
+
 		return craft()->templates->render(
 			'simplemap/marker/settings', array(
-				'settings' => $this->getSettings()
+				'settings' => $this->getSettings(),
+				'id' => $namespacedId
 			)
 		);
 	}
@@ -54,6 +58,13 @@ class SimpleMap_MarkerFieldType extends BaseFieldType
 	public function defineContentAttribute()
 	{
 		return AttributeType::Mixed;
+	}
+
+	private function addAssets()
+	{
+		craft()->templates->includeJsFile('//maps.googleapis.com/maps/api/js?sensor=false&libraries=places');
+		craft()->templates->includeJsResource('simplemap/js/SimpleMarker.js');
+		craft()->templates->includeCssResource('simplemap/css/global.css');
 	}
 
 }
--- simplemap/resources/js/SimpleMarker.js
+++ simplemap/resources/js/SimpleMarker.js
@@ -1,75 +1,120 @@
-var marker;
+
 
 $.fn.simpleMap = function() {
-    return this.each(function() {
-        console.log(this);
-    });
-};
 
-function initialize() {
+	var markers = [];
+	var maps = [];
 
-	var centerPosition = new google.maps.LatLng(defaultLat,defaultLong);
-	// var centerPosition = new google.maps.LatLng({{ settings.latitude }},{{ settings.longitude }});
-	var myOptions = {
-		zoom: defaultZoom,
-		center: centerPosition,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		draggable:true
-	};
-
-	map = new google.maps.Map(document.getElementById('fields-map_canvas-'+mapContainer), myOptions);
-
-	if(latitude && longitude)
-	{
-		var location = new google.maps.LatLng(latitude,longitude);
-		placeMarker(location);
-	}
-
-	google.maps.event.addListener(map, 'click', function(event) {
-	   placeMarker(event.latLng);
+	return this.each(function() {
+		google.maps.event.addDomListener(window, 'load', initialize( this.id ));
 	});
 
-}
+	function initialize(id) {
 
-function placeMarker(location) {
+		var field = $('#'+id);
+		var searchField = field.find('.map-address-search input');
+		var defaultLat  = field.data('defaultlat');
+		var defaultLong = field.data('defaultlong');
+		var defaultZoom = parseInt(field.data('defaultzoom'));
+		var latitude    = field.data('latitude');
+		var longitude   = field.data('longitude');
+		var mapElement  = field.find('.map_canvas_inner');
+		var centerPosition = new google.maps.LatLng(defaultLat,defaultLong);
 
-	 updateInputs( location.lat().toFixed(6), location.lng().toFixed(6) );
+		console.log(defaultZoom);
 
-    if (marker) {
-        //if marker already was created change positon
-        marker.setPosition(location);
-        
-    } else {
-        //create a marker
-        marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            draggable: true
-        });
-        // add drag listener
-        google.maps.event.addListener(marker, 'dragend', function(evt){
-		    updateInputs( evt.latLng.lat().toFixed(6), evt.latLng.lng().toFixed(6) );
+		var myOptions = {
+			zoom: defaultZoom,
+			center: centerPosition,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			draggable:true
+		};
+
+		maps[id] = new google.maps.Map( mapElement[0], myOptions);
+
+		if(latitude && longitude)
+		{
+			var location = new google.maps.LatLng(latitude,longitude);
+			placeMarker(location, id);
+		}
+
+		google.maps.event.addListener(maps[id], 'click', function(event) {
+			placeMarker(event.latLng, id);
 		});
-    }
-   // map.setCenter(marker.getPosition());
-    map.panTo(marker.getPosition());
-}
 
-function updateInputs(lat, lng){
-	$('#fields-location-latitude').val( lat );
-    $('#fields-location-longitude').val( lng );
-}
+		google.maps.event.addListener(markers[id],'dragend',function(event){
+			placeMarker(event.latLng, id);
+		});
 
-google.maps.event.addDomListener(window, 'load', initialize);
+		google.maps.event.addListener(maps[id],'zoom_changed',function(event){
+			$('#' + id + ' .location-zoom').val( maps[id].getZoom() );
+		});
 
-var autocomplete = new google.maps.places.Autocomplete($("#fields-address-search-string")[0]);
+		var autocomplete = new google.maps.places.Autocomplete( searchField[0] );
 
-google.maps.event.addListener(autocomplete, 'place_changed', function () {
+		google.maps.event.addListener(autocomplete, 'place_changed', function () {
 
-   var place = autocomplete.getPlace();
+			var place = autocomplete.getPlace();
 
-   if (typeof place.geometry.location != 'undefined') {
-   		placeMarker(place.geometry.location);
-   }
+			if (typeof place.geometry.location != 'undefined') {
+				placeMarker(place.geometry.location, id);
+			}
 
-});
+		});
+
+		$('#' + id + ' .location-latitude, #' + id + ' .location-longitude').change( function(e) {
+			var latitude = $('#' + id + ' .location-latitude').val();
+			var longitude = $('#' + id + ' .location-longitude').val();
+			var location = new google.maps.LatLng(latitude,longitude);
+			placeMarker(location, id);
+		});
+
+		// @todo workaround for map not rendering correctly
+		// when selecting this fieldtype in the fieldtype settings.
+		$('select.fieldtoggle').change( function(e) {
+			google.maps.event.trigger(maps[id], 'resize');
+			maps[id].panTo(markers[id].getPosition());
+		})
+
+		$('#' + id + ' .location-zoom').change( function(e) {
+			var zoom = $(this).val();
+			if(zoom) maps[id].setZoom( +zoom ); 
+		});
+		
+		
+		
+
+	}
+
+
+	function placeMarker(location, id) {
+
+		updateInputs( location.lat().toFixed(6), location.lng().toFixed(6), id );
+
+		if (markers[id]){
+			markers[id].setPosition(location);
+		} else{
+			markers[id] = new google.maps.Marker({
+				position: location,
+				map: maps[id],
+				draggable: true
+			});
+			google.maps.event.addListener(markers[id], 'dragend', function(evt){});
+		}
+		maps[id].panTo(markers[id].getPosition());
+	}
+
+	function updateInputs(lat, lng, id){
+		$('#' + id + ' .location-latitude').val( lat );
+		$('#' + id + ' .location-longitude').val( lng );
+		$('#' + id + ' .location-zoom').val( maps[id].getZoom() );
+	}
+
+};
+
+
+
+
+
+
+
